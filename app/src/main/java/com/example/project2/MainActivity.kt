6a6ca9fc8b2c40921cdbef4caa381a07906555dd
fatih <fatihnzrdn@gmail.com>
2023-05-09 14:50:08 +0700

    update switch

--- app/src/main/java/com/example/project2/MainActivity.kt
+++ app/src/main/java/com/example/project2/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.project2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //test dari patih
+        var text = findViewById<TextView>(R.id.textView)
+        var switchLampu = findViewById<Switch>(R.id.switch1)
+
+       switchLampu.setOnCheckedChangeListener
+
+
     }
 }
+
+
